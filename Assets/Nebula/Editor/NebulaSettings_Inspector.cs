0c59a750ccc3d7c830d0df97582762cb215a8186
Brian-E-Taylor <taylor.brian.eric@gmail.com>
2022-04-17 18:38:01 -0400

    Add saving/loading NebulaSettings to/from JSON file functionality.

--- Assets/Nebula/Editor/NebulaSettings_Inspector.cs
+++ Assets/Nebula/Editor/NebulaSettings_Inspector.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using UnityEditor;
 using UnityEngine.UIElements;
 
@@ -6,12 +8,60 @@ public class NebulaSettings_Inspector : Editor
 {
     public VisualTreeAsset inspectorXML;
 
+    private string _lastUsedDirectory;
+
+    void OnEnable()
+    {
+        _lastUsedDirectory = "";
+    }
+
     public override VisualElement CreateInspectorGUI()
     {
         var myInspector = new VisualElement();
 
         inspectorXML.CloneTree(myInspector);
+        myInspector.Q<Button>("SaveToJSONFileButton").clicked += SaveToJSONFile;
+        myInspector.Q<Button>("LoadFromJSONFileButton").clicked += LoadFromJSONFile;
 
         return myInspector;
+    }
+
+    private void SaveToJSONFile()
+    {
+        var path = EditorUtility.SaveFilePanel(
+            "Save Nebula Settings as JSON",
+            _lastUsedDirectory,
+            "nebulaSettings.json",
+            "json"
+        );
+
+        if (path.Length != 0)
+        {
+            // save to json
+            var nebulaSettings = target as NebulaSettings;
+            var jsonString = EditorJsonUtility.ToJson(nebulaSettings);
+            File.WriteAllText(path, jsonString);
+
+            _lastUsedDirectory = Path.GetDirectoryName(path);
+        }
+    }
+
+    private void LoadFromJSONFile()
+    {
+        var path = EditorUtility.OpenFilePanel(
+            "Load Nebula Settings from JSON",
+            _lastUsedDirectory,
+            "json");
+
+        if (path.Length != 0)
+        {
+            // load from json
+            var nebulaSettings = target as NebulaSettings;
+            var jsonString = File.ReadAllText(path);
+            EditorJsonUtility.FromJsonOverwrite(jsonString, nebulaSettings);
+            EditorUtility.SetDirty(nebulaSettings);
+
+            _lastUsedDirectory = Path.GetDirectoryName(path);
+        }
     }
 }
